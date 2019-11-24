34c2127d515a95316e3b9bdc2f1cc4e7f5e40583
rame0 <r@me0.biz>
2019-11-24 22:15:18 +0300

    Немного причесал код

--- test_remove_bg_final.py
+++ test_remove_bg_final.py
@@ -3,32 +3,25 @@ import cv2
 import _utils
 from matplotlib import pyplot as plt
 
-largestSideSize = 500
+smallestSideSize = 500
 # real would be thicker because of masking process
 mainRectSize = .04
 fgSize = .15
 
-img = cv2.imread('img_test/1.jpg')
+img = cv2.imread('img_test/10.jpg')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-# quantify colors
-# img = _utils.quantify_colors(img, 32, 10)
 
 height, width = img.shape[:2]
-
-new_w = width
-new_h = height
-if largestSideSize > 0 and width > height:
-    new_h = largestSideSize
-    new_w = round(width * new_h / height)
-elif largestSideSize > 0:
-    new_w = largestSideSize
-    new_h = round(height * new_w / width)
-
+new_w, new_h = _utils.new_image_size(width, height, smallestSideSize)
 
 # resize image to lower resources usage
 # if you need masked image in original size, do not resize it
 img_small = cv2.resize(img, (new_w, new_h), interpolation=cv2.INTER_AREA)
+
+# quantify colors
+# img_small = _utils.quantify_colors(img_small, 32, 10)
+
 # create mask tpl
 mask = np.zeros(img_small.shape[:2], np.uint8)
 
@@ -43,21 +36,19 @@ fg_h = round(new_h * (1 - fgSize) / 2)
 fg_rect = (fg_w, fg_h, new_w - fg_w, new_h - fg_h)
 
 # color: 0 - bg, 1 - fg, 2 - probable bg, 3 - probable fg
-cv2.rectangle(mask, fg_rect[:2], fg_rect[2:4], color=1, thickness=-1)
+cv2.rectangle(mask, fg_rect[:2], fg_rect[2:4], color=cv2.GC_FGD, thickness=-1)
 
 mask_preset = mask.copy()
 
 bgdModel1 = np.zeros((1, 65), np.float64)
 fgdModel1 = np.zeros((1, 65), np.float64)
-bgdModel2 = np.zeros((1, 65), np.float64)
-fgdModel2 = np.zeros((1, 65), np.float64)
 
-# just to visualize
-mask_before = mask.copy()
 cv2.grabCut(img_small, mask, bg_rect, bgdModel1, fgdModel1, 3, cv2.GC_INIT_WITH_RECT)
+mask_rect = mask.copy()
 
-cv2.rectangle(mask, bg_rect[:2], bg_rect[2:4], color=2, thickness=bg_w * 3)
+cv2.rectangle(mask, bg_rect[:2], bg_rect[2:4], color=cv2.GC_PR_BGD, thickness=bg_w * 3)
 cv2.grabCut(img_small, mask, bg_rect, bgdModel1, fgdModel1, 10, cv2.GC_INIT_WITH_MASK)
+mask_mask = mask.copy()
 
 # mask to remove background
 mask_result = np.where((mask == 1) + (mask == 3), 255, 0).astype('uint8')
@@ -65,8 +56,8 @@ mask_result = np.where((mask == 1) + (mask == 3), 255, 0).astype('uint8')
 # if we are removing too much, assume there is no background
 unique, counts = np.unique(mask_result, return_counts=True)
 mask_dict = dict(zip(unique, counts))
-print(mask_dict[0], mask_dict[255] * 1.5)
-if mask_dict[0] > mask_dict[255] * 1.5:
+
+if mask_dict[0] > mask_dict[255] * 1.6:
     mask_result = np.where((mask == 0) + (mask != 1) + (mask != 3), 255, 0).astype('uint8')
 
 # apply mask to image
@@ -77,14 +68,11 @@ masked[mask_result < 2] = [0, 0, 255]  # change black bg to blue
 cv2.rectangle(img_small, bg_rect[:2], bg_rect[2:4], (255, 0, 0), 2)
 cv2.rectangle(img_small, fg_rect[:2], fg_rect[2:4], (0, 255, 0), 2, cv2.FILLED)
 
-plt.subplot(2, 3, 1), plt.imshow(img_small), plt.title("Orig img")
-plt.subplot(2, 3, 2), plt.imshow(masked), plt.title("Result")
-plt.subplot(2, 3, 4), plt.imshow(mask_before), plt.title("Mask orig")
-plt.subplot(2, 3, 5), plt.imshow(mask), plt.title("Mask preset")
-plt.subplot(2, 3, 6), plt.imshow(mask_result), plt.title("Mask result")
-
-# save result
-masked = cv2.cvtColor(masked, cv2.COLOR_RGB2BGR)
-cv2.imwrite("out/1.png", masked)
+plt.subplot(2, 3, 1), plt.imshow(img_small), plt.title("Orig img with preset")
+plt.subplot(2, 3, 2), plt.imshow(mask_result), plt.title("Final Mask")
+plt.subplot(2, 3, 3), plt.imshow(masked), plt.title("Result")
+plt.subplot(2, 3, 4), plt.imshow(mask_preset), plt.title("Mask preset FG")
+plt.subplot(2, 3, 5), plt.imshow(mask_rect), plt.title("GC_INIT_WITH_RECT")
+plt.subplot(2, 3, 6), plt.imshow(mask_mask), plt.title("GC_INIT_WITH_MASK")
 
 plt.show()
